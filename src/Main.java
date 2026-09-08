82f3d3851a9f4c8ba16ebccb3706c6fadcf71dd2
danil <danillinnik85@list.ru>
2026-09-08 23:17:20 +0300

    Исправленное ДЗ x2

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-      System.out.println("Задача 1");
+      System.out.println("Задача №1");
       for (int i = 0; i < 10; i = i + 1){
           System.out.println("Итерация цикла " + i);
       }
